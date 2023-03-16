45b5829c6b0dbf2dd6bf42969f1281719340a8f0
mell <126731231+mellcs@users.noreply.github.com>
2023-03-16 20:23:53 -0300

    Add files via upload

---
+++ semana01/input.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome")
+print ("Seu nome é", nome)
+
+idade = int (input("Digite sua idade"))
+print("Sua idade é", idade, "anos")
+
+peso = float ( input("Digite seu peso",))
+print ("Seu peso é ", peso, "kg")
+
+peso = peso + 5
