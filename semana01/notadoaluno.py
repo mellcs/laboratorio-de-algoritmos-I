d66f969fe05f939caa837fc18b3686beb2831699
mell <126731231+mellcs@users.noreply.github.com>
2023-03-18 20:44:59 -0300

    Delete notadoaluno.py

--- semana01/notadoaluno.py
+++
@@ -1,9 +0,0 @@
-nome = input("Nome do aluno")
-
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-n3 = float(input("Digite a terceira nota: "))
-
-media = (n1 + n2 + n3) / 3
-
-print ("Parabéns, sua nota foi: ", media)
