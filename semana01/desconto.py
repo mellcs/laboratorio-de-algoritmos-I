863ada3ab21dde6abd3c8f0e1fed1484ada30355
mell <126731231+mellcs@users.noreply.github.com>
2023-03-18 20:22:55 -0300

    Delete desconto.py

--- semana01/desconto.py
+++
@@ -1,6 +0,0 @@
-nome = (input("Insira seu nome: "))
-valorDoProduto = float(input("Insira o valor original do produto"))
-#valorfinal = valordefarica * 1.65
-#valorFinal = valorDoProduto * 165/100 
-valorFinal = (valorDoProduto * 0.65) + valorDoProduto
-print ("O valor final do produto será de ", valorFinal)
