0510f55b75419f6b31c1f9f6ae3aaaa704a20c4b
mell <126731231+mellcs@users.noreply.github.com>
2023-03-23 19:55:05 -0300

    Add files via upload

---
+++ semana03/if e else 2.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite um valor:"))
+n2 = int(input("Digite outro valor:"))
+
+if n1>n2:
+    print("O valor:", n1, "é maior que", n2)
+else:
+    print("O valor:", n2, "é maior que", n1)
