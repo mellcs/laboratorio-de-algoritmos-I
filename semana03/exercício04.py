a71c4484371b5a365e739fd53c2ef91cc48441c3
mell <126731231+mellcs@users.noreply.github.com>
2023-03-23 21:19:42 -0300

    Add files via upload

---
+++ semana03/exercício04.py
@@ -0,0 +1,7 @@
+precoDaGasolina = float(input("Insira o preço da gasolina:"))
+
+valor = float(input("Insira o valor desejado:"))
+
+valorTotal = valor / precoDaGasolina
+
+print("Os litros colocados foram:", valorTotal)
