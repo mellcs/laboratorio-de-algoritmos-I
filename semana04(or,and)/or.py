9200404126660504fe9c98767665ae6293382641
mell <126731231+mellcs@users.noreply.github.com>
2023-03-30 22:02:49 -0300

    Add files via upload

---
+++ semana04(or,and)/or.py
@@ -0,0 +1,6 @@
+letra = input("Digite uma letra:").upper()
+
+if letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U":
+    print("A letra é vogal.")
+else:
+    print("A letra é consoante.")
