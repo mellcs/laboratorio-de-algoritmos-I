7eb3eb58f129041318932fa36f673aa68c557318
mell <126731231+mellcs@users.noreply.github.com>
2023-03-30 21:59:05 -0300

    Add files via upload

---
+++ semana04(or,and)/and.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite a sua idade:"))
+if idade >= 18 and idade > 70:
+    print("Você é obrigado a votar!")
+elif idade >= 16 or idade >= 70:
+    ("Você não é obrigado a votar!")
+else:
+    print("Você não pode votar!")
