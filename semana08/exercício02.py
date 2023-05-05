ebe43e6414a3ab5882487317204992599fcb1ed8
mell <126731231+mellcs@users.noreply.github.com>
2023-05-04 21:28:04 -0300

    Add files via upload

---
+++ semana08/exercício02.py
@@ -0,0 +1,16 @@
+
+valor = int(input("Insira um valor:"))
+
+def retornarDobro(valor):
+    dobro = valor * 2
+    return dobro
+dobro = retornarDobro(valor)
+print(dobro)
+
+def retornarTriplo(valor):
+    triplo = valor * 3
+    return triplo
+triplo = retornarTriplo(valor)
+print(triplo)
+
+main()
